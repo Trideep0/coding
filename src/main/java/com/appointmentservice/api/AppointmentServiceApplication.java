f07390716f34504064e25d6221449affab87e9e6
Trideep0 <dp0964168@gmail.com>
2023-10-30 19:28:29 +0530

    this my first commit

---
+++ src/main/java/com/appointmentservice/api/AppointmentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.appointmentservice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppointmentServiceApplication.class, args);
+		System.out.println("Sonu");
+	}
+
+}
